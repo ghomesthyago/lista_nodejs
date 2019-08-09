998cc3119ebcaf10e206eed882ecb3489233d78d
ghomesthyago <ghomes.thyago@gmail.com>
2019-08-09 08:24:19 -0300

    Instalação do MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+//Criando Rota
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
 
 app.listen(3001);
